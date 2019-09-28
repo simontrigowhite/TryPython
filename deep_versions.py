8bec4b9eac94263c1f54bd1b3548be58cbd770fb
Simon White <simon@trigowhite.com>
2019-09-28 22:45:24 +0100

    deep learning with keras

---
+++ deep_versions.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 28 21:58:04 2019
+
+@author: Simon
+"""
+
+# https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
+#
+# conda install theano
+# pip install tensorflow
+# pip install keras
+#
+
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+
+# keras
+import keras
+print('keras: %s' % keras.__version__)
--- plots.py
+++ plots.py
@@ -8,12 +8,19 @@ Created on Sat Sep 28 21:17:40 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 10, 0.01)
+x = np.arange(0, 20, 0.01)
 y = np.sin(x)
+z = np.cos(x)
 
 fig_size = plt.rcParams["figure.figsize"]
-fig_size[0] = 18
-fig_size[1] = 12
+fig_size[0] = 10
+fig_size[1] = 5
 plt.rcParams["figure.figsize"] = fig_size
 
-plt.plot(x, y)
+plt.plot(x, y, label="sin")
+plt.plot(x, z, label="cos")
+plt.legend(loc='upper center')
+
+plt.xticks(np.arange(min(x), max(x)+1, np.pi / 2))
+
+plt.show()
