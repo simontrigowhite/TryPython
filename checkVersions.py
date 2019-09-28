50b797d6cc03face71d75bf3d927f22e15ae5a5b
Simon White <simon@trigowhite.com>
2019-09-28 19:40:06 +0100

    evaluate some algorithms

--- checkVersions.py
+++ checkVersions.py
@@ -5,24 +5,30 @@ Created on Fri Sep 27 22:45:28 2019
 @author: Simon
 """
 
+# https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
 # Check the versions of libraries
 
 # Python version
 import sys
 print('Python: {}'.format(sys.version))
+
 # scipy
 import scipy
 print('scipy: {}'.format(scipy.__version__))
+
 # numpy
 import numpy
 print('numpy: {}'.format(numpy.__version__))
+
 # matplotlib
 import matplotlib
 print('matplotlib: {}'.format(matplotlib.__version__))
+
 # pandas
 import pandas
 print('pandas: {}'.format(pandas.__version__))
+
 # scikit-learn
 import sklearn
 print('sklearn: {}'.format(sklearn.__version__))
-
--- loadData.py
+++ loadData.py
@@ -5,20 +5,12 @@ Created on Fri Sep 27 22:48:37 2019
 @author: Simon
 """
 
+# https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
 # Load libraries
 import pandas
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 
 # Load dataset
 url = "C:/Users/Simon/Documents/GitHub/TryPython/iris.csv"
@@ -28,7 +20,6 @@ dataset = pandas.read_csv(url, names=names)
 # shape
 print(dataset.shape)
 
-
 # head
 print(dataset.head(20))
 
@@ -36,10 +27,11 @@ print(dataset.head(20))
 print(dataset.describe())
 
 # class distribution
-print(dataset.groupby('class').size())
+print(dataset.groupby('class', sort=False).size())
 
 # box and whisker plots
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+dataset.plot(
+        kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
 plt.show()
 
 # histograms
@@ -49,4 +41,3 @@ plt.show()
 # scatter plot matrix
 scatter_matrix(dataset)
 plt.show()
-
